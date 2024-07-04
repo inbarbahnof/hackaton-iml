9035bf6832613cb225d8362b5b353d56fdabcc28
inbarbahnof <158153825+inbarbahnof@users.noreply.github.com>
2024-07-04 17:08:37 +0300

    baseline second

--- main_dur.py
+++ main_dur.py
@@ -12,7 +12,7 @@ from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error, r2_score
 from sklearn.preprocessing import PolynomialFeatures
 from sklearn.tree import DecisionTreeRegressor
-import evaluation_scripts.eval_passengers_up as eval
+import evaluation_scripts.eval_trip_duration as eval
 
 TRAIN_BUS_CSV_PATH = "data/train_bus_schedule.csv"
 X_PASSENGER = "data/X_passengers_up.csv"
@@ -64,23 +64,30 @@ def feature_evaluation(X: pd.DataFrame, y: pd.Series,
 
 def preprocessing_baseline(X: pd.DataFrame, y: pd.Series):
     # Save the trip_id_unique_station column
-    f_station_cnt =X.groupby("trip_id_unique")["trip_id_unique_station"].nunique().to_frame("station_cnt")
-    f_total_passenger = X.groupby("trip_id_unique")["passengers_up"].sum().to_frame("total_passenger")
-    f_mean_passenger = X.groupby("trip_id_unique")["passengers_up"].mean().to_frame("mean_passenger")
-    f_mean_passenger_c = X.groupby("trip_id_unique")["passengers_continue"].mean().to_frame("mean_passenger_c")
+    f_station_cnt = X.groupby("trip_id_unique")["trip_id_unique_station"].nunique().to_frame(
+        "station_cnt")
+    f_total_passenger = X.groupby("trip_id_unique")["passengers_up"].sum().to_frame(
+        "total_passenger")
+    f_mean_passenger = X.groupby("trip_id_unique")["passengers_up"].mean().to_frame(
+        "mean_passenger")
+    f_mean_passenger_c = X.groupby("trip_id_unique")["passengers_continue"].mean().to_frame(
+        "mean_passenger_c")
     f_start_time = X.groupby("trip_id_unique")["arrival_time"].min().to_frame("start_time")
     f_start_time['start_time'] = pd.to_datetime(f_start_time['start_time']).dt.hour
-    features = pd.concat([f_station_cnt,f_total_passenger, f_mean_passenger, f_mean_passenger_c, f_start_time], axis=1)
-    features = features.merge(dur_baseline[["trip_id_unique","cluster","direction","mekadem_nipuach_luz"]],on = "trip_id_unique")
+    features = pd.concat(
+        [f_station_cnt, f_total_passenger, f_mean_passenger, f_mean_passenger_c, f_start_time],
+        axis=1)
+    features = features.merge(
+        dur_baseline[["trip_id_unique", "cluster", "direction", "mekadem_nipuach_luz"]],
+        on="trip_id_unique")
     label_encoder = LabelEncoder()
     features['cluster'] = label_encoder.fit_transform(features['cluster'])
-    features = features.merge(y[["trip_id_unique","delta"]],on ="trip_id_unique" )
-    features = features.drop_duplicates ()
-    X_duration = features.drop(columns= ["delta","trip_id_unique"])
+    features = features.merge(y[["trip_id_unique", "delta"]], on="trip_id_unique")
+    features = features.drop_duplicates()
+    X_duration = features.drop(columns=["delta"])
     Y_duration = features["delta"]
-    X_train,X_test,y_train,y_test = sk.train_test_split(X_duration,Y_duration,train_size=0.75,random_state=RANDOM_STATE)
-    X_train = X_train.set_index("trip_id_unique")
-    X_test = X_test.set_index("trip_id_unique")
+    X_train, X_test, y_train, y_test = sk.train_test_split(X_duration, Y_duration, train_size=0.75,
+                                                           random_state=RANDOM_STATE)
     return X_train, X_test, y_train, y_test
 
 
@@ -108,20 +115,23 @@ def linear_regression(X_train: pd.DataFrame, X_test: pd.DataFrame, y_train: pd.S
 
     # Create DataFrames for evaluation
     predictions_df = pd.DataFrame({
-        'trip_id_unique': X_test.index,  # Ensure we have the correct index for trip_id_unique_station
-        'dur_x': y_pred
+        'trip_id_unique': X_test.index,
+        # Ensure we have the correct index for trip_id_unique_station
+        'trip_duration_in_minutes_x': y_pred
     })
 
     ground_truth_df = pd.DataFrame({
-        'trip_id_unique': X_test.index,  # Ensure we have the correct index for trip_id_unique_station
-        'dur_y': y_test.values
+        'trip_id_unique': X_test.index,
+        # Ensure we have the correct index for trip_id_unique_station
+        'trip_duration_in_minutes_y': y_test.values
     })
 
     # Evaluate using eval_boardings
-    mse_board = eval.eval_boardings(predictions_df, ground_truth_df)
+    mse_board = eval.eval_duration(predictions_df, ground_truth_df)
     return mse_board
 
-def csv_output(mse:float ,passengers_up: pd.Series, trip_id_unique_station):
+
+def csv_output(mse: float, passengers_up: pd.Series, trip_id_unique_station):
     # Create DataFrame with predictions
     predictions_df = pd.DataFrame({
         'trip_id_unique_station': trip_id_unique_station,
@@ -131,13 +141,15 @@ def csv_output(mse:float ,passengers_up: pd.Series, trip_id_unique_station):
     # Save predictions to CSV file
     predictions_df.to_csv('passengers_up_predictions.csv', index=False)
 
+
 def __creating_labels(dur_baseline):
-    min_max_time = dur_baseline.groupby("trip_id_unique")["arrival_time"].agg({"min","max"}).reset_index()
+    min_max_time = dur_baseline.groupby("trip_id_unique")["arrival_time"].agg(
+        {"min", "max"}).reset_index()
     min_max_time["max"] = pd.to_datetime(min_max_time["max"])
     min_max_time["min"] = pd.to_datetime(min_max_time["min"])
-    min_max_time["delta"] = (min_max_time["max"] - min_max_time["min"])/pd.Timedelta(1,"m")
-    min_max_time["delta"] =round(min_max_time["delta"],2)
-    return min_max_time[["trip_id_unique","delta"]]
+    min_max_time["delta"] = (min_max_time["max"] - min_max_time["min"]) / pd.Timedelta(1, "m")
+    min_max_time["delta"] = round(min_max_time["delta"], 2)
+    return min_max_time[["trip_id_unique", "delta"]]
 
 
 if __name__ == '__main__':
@@ -153,8 +165,9 @@ if __name__ == '__main__':
     # 1. load the training set (args.training_set)
     train_bus = pd.read_csv(TRAIN_BUS_CSV_PATH, encoding=ENCODER)
     x_trip_duration = pd.read_csv(X_TRIP, encoding=ENCODER)
-    lines_for_baseline = train_bus["trip_id_unique"].drop_duplicates().sample(frac = 0.10,random_state= RANDOM_STATE)
-    dur_baseline  = train_bus[train_bus["trip_id_unique"].isin(lines_for_baseline)]
+    lines_for_baseline = train_bus["trip_id_unique"].drop_duplicates().sample(frac=0.10,
+                                                                              random_state=RANDOM_STATE)
+    dur_baseline = train_bus[train_bus["trip_id_unique"].isin(lines_for_baseline)]
     dur_baseline = dur_baseline[x_trip_duration.columns]
     dur_labels = __creating_labels(dur_baseline)
 
@@ -176,4 +189,3 @@ if __name__ == '__main__':
 
     # 7. save the predictions to args.out
     # logging.info("predictions saved to {}".format(args.out))
-
